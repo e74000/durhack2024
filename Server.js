9d9889a18e00eab2600629d952de05781a1ec954
Dylan Wilde <dwilde2005@gmail.com>
2024-11-02 20:36:42 +0000

    Add ignore for node_modules directory

---
+++ Server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const path = require('path');
+
+app.get('/', (req, res) => {
+    try {
+        res.status(200).sendFile(path.join(__dirname + '/index.html'));
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    console.log('Server is running on port http://localhost:8080');
+
+});

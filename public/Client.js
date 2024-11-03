79fcf8535b86404d983dd84a366b5e876f643f86
Dylan Wilde <dwilde2005@gmail.com>
2024-11-03 09:19:41 +0000

    Refactor code to display data in table

--- public/Client.js
+++ public/Client.js
@@ -7,35 +7,45 @@ async function loadData() {
         }
 
         const data = await response.json();
+        console.log('loaded in loadData');
         console.log(data);
         //// bit to display data not written yet
+        return data;
     }
     catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
         // display error message 
     }
-
 }
-
 // create event listener for dropdown menu
 // will need to have a button to set a date submission thing
 
-function display_data(){
-    var data = loadData();
-    console.log(data);
+async function displayData(){
+    let tableHTML = "<table><thead><tr><th>Predicted mean temp</th></tr></thead><tbody>";
+    Mon = document.getElementById('Monday');
+    loadData().then (function(data){
+        console.log('data loaded in display_data');
+        console.log(data);
+        // create a table
+        tableHTML += "<tr><td>" + data.Monday.Prediction_Mean_Temp + "</td></tr>";
+        console.log(data.Monday.Prediction_Mean_Temp);
+        console.log(data.Monday.Predicted_Mean_Temp);
+        // display data
+        // create a variable to write the data to in the html
+        // create a table to display data, then append to the body of the page when it exists
+        // create a loop to iterate through the data and write it to the table
+
+        // create a table
+
+    });
+
+    tableHTML += "</tbody></table>";
+    Mon.innerHTML = tableHTML;
     // display data
     // create a variable to write the data to in the html
     // create a table to display data, then append to the body of the page when it exists
     // create a loop to iterate through the data and write it to the table
-
-    data.forEach(function(data) => {
-        // write data to table
-        // table will have to created here and appended to the body of the page
-        // change htmlinner for things with id =  panel[x]
-
-        
-    });
 }
-display_data();
+displayData();
 
 
